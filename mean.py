f29057ba3e62a83d6e77eecb57511008c2c4187b
arthur <arthur.teisseire@epitech.eu>
2019-04-23 10:31:20 +0200

    Arithmetic done

--- mean.py
+++ mean.py
@@ -14,14 +14,15 @@ def get_input(n, a, h, sd):
 
 def mean(n, a, h, sd, value):
     n += 1
+    a = (a * (n - 1) + value) / n
     print_all(n, a, h, sd)
     return n, a, h, sd
 
 
 def print_all(n, a, h, sd):
     print("\tNumber of values:\t%d" % n)
-    print("\tStandard deviation:\t%d" % sd)
-    print("\tArithmetic mean:\t%d" % a)
-    print("\tRoot mean square:\t0")
-    print("\tHarmonic mean:\t\t%d" % h)
+    print("\tStandard deviation:\t%.2f" % sd)
+    print("\tArithmetic mean:\t%.2f" % a)
+    print("\tRoot mean square:\t%.2f")
+    print("\tHarmonic mean:\t\t%.2f" % h)
     print("")
