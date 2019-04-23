e767178235057cb8fbdd7775fb3f3f34d80723ef
arthur <arthur.teisseire@epitech.eu>
2019-04-23 10:22:12 +0200

    Return 4 values

--- mean.py
+++ mean.py
@@ -1,15 +1,17 @@
 def algo(n, a, h, sd):
     while True:
-        get_input(n, a, h, sd)
+        n, a, h, sd = get_input(n, a, h, sd)
 
 
 def get_input(n, a, h, sd):
     value = input("Enter next value: ")
     try:
-        mean(n, a, h, sd, int(value))
+        return mean(n, a, h, sd, int(value))
     except ValueError as e:
         print(e)
+        return n, a, h, sd
 
 
 def mean(n, a, h, sd, value):
     print(value)
+    return n, a, h, sd
