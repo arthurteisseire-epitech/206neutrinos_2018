2fb657275f1b5b185f34de20aac3549079b8aa6b
arthur <arthur.teisseire@epitech.eu>
2019-04-23 10:19:28 +0200

    Extract try in another function

--- mean.py
+++ mean.py
@@ -1,11 +1,15 @@
 def algo(n, a, h, sd):
     while True:
-        mean(n, a, h, sd)
+        get_input(n, a, h, sd)
 
 
-def mean(n, a, h, sd):
+def get_input(n, a, h, sd):
     value = input("Enter next value: ")
     try:
-        value = int(value)
+        mean(n, a, h, sd, int(value))
     except ValueError as e:
         print(e)
+
+
+def mean(n, a, h, sd, value):
+    print(value)
