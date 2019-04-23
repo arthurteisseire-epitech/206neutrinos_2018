abdec506fae14c3b4e348cfab5bf804f9da983e6
arthur <arthur.teisseire@epitech.eu>
2019-04-23 10:50:01 +0200

    sd not working

--- mean.py
+++ mean.py
@@ -1,3 +1,6 @@
+from math import sqrt
+
+
 def algo(n, a, h, sd):
     while True:
         n, a, h, sd = get_input(n, a, h, sd)
@@ -15,14 +18,15 @@ def get_input(n, a, h, sd):
 def mean(n, a, h, sd, value):
     n += 1
     a = (a * (n - 1) + value) / n
-    print_all(n, a, h, sd)
+    sd = sqrt(pow(a, 2) / (n - 1))
+    print_all(n, a, h, sd, 0)
     return n, a, h, sd
 
 
-def print_all(n, a, h, sd):
+def print_all(n, a, h, sd, rms):
     print("\tNumber of values:\t%d" % n)
     print("\tStandard deviation:\t%.2f" % sd)
     print("\tArithmetic mean:\t%.2f" % a)
-    print("\tRoot mean square:\t%.2f")
+    print("\tRoot mean square:\t%.2f" % rms)
     print("\tHarmonic mean:\t\t%.2f" % h)
     print("")
