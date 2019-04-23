3d1b8868bf5084d7a593d8fd08b2c615591fba88
arthur <arthur.teisseire@epitech.eu>
2019-04-23 10:00:51 +0200

    Add parsing

---
+++ parse.py
@@ -0,0 +1,11 @@
+import argparse
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("n", type=int, help="number of values")
+    parser.add_argument("e", type=int, help="arithmetic mean")
+    parser.add_argument("h", type=int, help="harmonic mean")
+    parser.add_argument("sd", type=int, help="standard deviation")
+    args = parser.parse_args()
+    return args
