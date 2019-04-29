27105d24965debb90efd04bde190b74922585e6f
Mahe Caurette <mahe.caurette@epitech.eu>
2019-04-29 09:36:16 +0100

    blip bloup your mother eat poop

--- mean.py
+++ mean.py
@@ -18,12 +18,14 @@ def get_input(n, a, h, sd):
 
 
 def mean(n, a, h, sd, value):
+    old_sd = sd
+    old_a = a
     sd = sqrt((n / (n + 1)) * (pow(sd, 2) + (pow(value - a, 2) / (n + 1))))
     a = (a * n + value) / (n + 1)
     n += 1
-    exisquare = (n - 1) * (pow(sd, 2) + pow(a, 2))
-    rms = sqrt((exisquare + pow(value, 2)) / n)
     h = n / ((1 / value) + ((n - 1) / h))
+    exisquare = (n - 1) * (pow(old_sd, 2) + pow(old_a, 2))
+    rms = sqrt((exisquare + pow(value, 2)) / n)
     print_all(n, a, h, sd, rms)
     return n, a, h, sd
 
--- parse.py
+++ parse.py
@@ -8,11 +8,18 @@ def positive_int(n):
     return n
 
 
+def non_null_positive_int(n):
+    n = int(n)
+    if n <= 0:
+        raise argparse.ArgumentTypeError("n and sd must be positive int")
+    return n
+
+
 def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("n", type=positive_int, help="number of values")
     parser.add_argument("a", type=int, help="arithmetic mean")
-    parser.add_argument("h", type=positive_int, help="harmonic mean")
+    parser.add_argument("h", type=non_null_positive_int, help="harmonic mean")
     parser.add_argument("sd", type=positive_int,  help="standard deviation")
     args = parser.parse_args()
     return args
