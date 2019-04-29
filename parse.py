3e7e59127d2b1b92502bdc64a49fe4c1c8844de4
Mahe Caurette <mahe.caurette@epitech.eu>
2019-04-29 09:16:06 +0100

    new parse for harmonic

--- parse.py
+++ parse.py
@@ -12,7 +12,7 @@ def parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("n", type=positive_int, help="number of values")
     parser.add_argument("a", type=int, help="arithmetic mean")
-    parser.add_argument("h", type=int, help="harmonic mean")
+    parser.add_argument("h", type=positive_int, help="harmonic mean")
     parser.add_argument("sd", type=positive_int,  help="standard deviation")
     args = parser.parse_args()
     return args
